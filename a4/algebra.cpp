8eecb15382d6958e4bc9d9c3edd68a25be63b993
jmah mba <jeremyw.mah@gmail.com>
2015-11-29 22:37:37 -0700

    intersection seems to work, scene is still funky

---
+++ a4/algebra.cpp
@@ -0,0 +1,339 @@
+//---------------------------------------------------------------------------
+//
+// CPSC453 -- Introduction to Computer Graphics
+// Assignment 2
+//
+// Classes and functions for manipulating points, vectors, matrices, 
+// and colours.  You probably won't need to modify anything in these
+// two files.
+//
+// Adapted from CS488 A2 University of Waterloo Computer Graphics Lab / 2003
+//
+//---------------------------------------------------------------------------
+
+#include "algebra.h"
+
+double Vector3D::normalize()
+{
+  double denom = 1.0;
+  double x = (v_[0] > 0.0) ? v_[0] : -v_[0];
+  double y = (v_[1] > 0.0) ? v_[1] : -v_[1];
+  double z = (v_[2] > 0.0) ? v_[2] : -v_[2];
+
+  if(x > y) {
+    if(x > z) {
+      if(1.0 + x > 1.0) {
+        y = y / x;
+        z = z / x;
+        denom = 1.0 / (x * sqrt(1.0 + y*y + z*z));
+      }
+    } else { /* z > x > y */ 
+      if(1.0 + z > 1.0) {
+        y = y / z;
+        x = x / z;
+        denom = 1.0 / (z * sqrt(1.0 + y*y + x*x));
+      }
+    }
+  } else {
+    if(y > z) {
+      if(1.0 + y > 1.0) {
+        z = z / y;
+        x = x / y;
+        denom = 1.0 / (y * sqrt(1.0 + z*z + x*x));
+      }
+    } else { /* x < y < z */
+      if(1.0 + z > 1.0) {
+        y = y / z;
+        x = x / z;
+        denom = 1.0 / (z * sqrt(1.0 + y*y + x*x));
+      }
+    }
+  }
+
+  if(1.0 + x + y + z > 1.0) {
+    v_[0] *= denom;
+    v_[1] *= denom;
+    v_[2] *= denom;
+    return 1.0 / denom;
+  }
+
+  return 0.0;
+}
+
+/*
+ * Define some helper functions for matrix inversion.
+ */
+
+static void swaprows(Matrix4x4& a, size_t r1, size_t r2)
+{
+  std::swap(a[r1][0], a[r2][0]);
+  std::swap(a[r1][1], a[r2][1]);
+  std::swap(a[r1][2], a[r2][2]);
+  std::swap(a[r1][3], a[r2][3]);
+}
+
+static void dividerow(Matrix4x4& a, size_t r, double fac)
+{
+  a[r][0] /= fac;
+  a[r][1] /= fac;
+  a[r][2] /= fac;
+  a[r][3] /= fac;
+}
+
+static void submultrow(Matrix4x4& a, size_t dest, size_t src, double fac)
+{
+  a[dest][0] -= fac * a[src][0];
+  a[dest][1] -= fac * a[src][1];
+  a[dest][2] -= fac * a[src][2];
+  a[dest][3] -= fac * a[src][3];
+}
+
+/*
+ * invertMatrix
+ *
+ * I lifted this code from the skeleton code of a raytracer assignment
+ * from a different school.  I taught that course too, so I figured it
+ * would be okay.
+ */
+Matrix4x4 Matrix4x4::invert() const
+{
+  /* The algorithm is plain old Gauss-Jordan elimination 
+     with partial pivoting. */
+
+  Matrix4x4 a(*this);
+  Matrix4x4 ret;
+
+  /* Loop over cols of a from left to right, 
+     eliminating above and below diag */
+
+  /* Find largest pivot in column j among rows j..3 */
+  for(size_t j = 0; j < 4; ++j) { 
+    size_t i1 = j; /* Row with largest pivot candidate */
+    for(size_t i = j + 1; i < 4; ++i) {
+      if(fabs(a[i][j]) > fabs(a[i1][j])) {
+        i1 = i;
+      }
+    }
+
+    /* Swap rows i1 and j in a and ret to put pivot on diagonal */
+    swaprows(a, i1, j);
+    swaprows(ret, i1, j);
+
+    /* Scale row j to have a unit diagonal */
+    if(a[j][j] == 0.0) {
+      // Theoretically throw an exception.
+      return ret;
+    }
+
+    dividerow(ret, j, a[j][j]);
+    dividerow(a, j, a[j][j]);
+
+    /* Eliminate off-diagonal elems in col j of a, doing identical 
+       ops to b */
+    for(size_t i = 0; i < 4; ++i) {
+      if(i != j) {
+        submultrow(ret, i, j, a[i][j]);
+        submultrow(a, i, j, a[i][j]);
+      }
+    }
+  }
+
+  return ret;
+}
+
+Line3D::Line3D() { }
+
+Line3D::Line3D(const Point3D &p1, Point3D &p2) : p1_(p1), p2_(p2) { }
+
+Line3D::Line3D(const Line3D &other) : p1_(other.p1_), p2_(other.p2_) { }
+
+Line3D::~Line3D() { }
+
+Point3D &Line3D::getP1()
+{
+    return p1_;
+}
+
+Point3D &Line3D::getP2()
+{
+    return p2_;
+}
+
+std::vector<Line3D> Triangle::getLines()
+{
+    std::vector<Line3D> lines;
+
+    for(std::vector<Point3D>::iterator it = verts_.begin(); it != verts_.end(); ++it)
+    {
+        Point3D p1 = *it, p2 = *(++it);
+        lines.push_back(Line3D(p1,p2));
+    }
+    return lines;
+}
+///////////
+
+Gnomon::Gnomon()
+{
+    verts_.push_back(Point3D(0,0,0));
+    verts_.push_back(Point3D(50,0,0));
+    verts_.push_back(Point3D(0,0,0));
+    verts_.push_back(Point3D(0,50,0));
+    verts_.push_back(Point3D(0,0,0));
+    verts_.push_back(Point3D(0,0,50));
+}
+
+Gnomon::~Gnomon() {}
+Matrix4x4 Gnomon::getTransform() const
+{
+    return this->translationTransform_ * rotationTransform_;
+}
+
+void Gnomon::resetTransform()
+{
+    this->translationTransform_ = Matrix4x4();
+    this->rotationTransform_ = Matrix4x4();
+}
+
+void Gnomon::appendTranslationTransform(const Matrix4x4 &xform)
+{
+    this->translationTransform_ = this->translationTransform_ * xform;
+}
+
+void Gnomon::appendRotationTransform(const Matrix4x4 &xform)
+{
+    this->rotationTransform_ = this->rotationTransform_ * xform;
+}
+
+std::vector<Line3D> Gnomon::getLines()
+{
+    std::vector<Line3D> lines;
+    for(std::vector<Point3D>::iterator it = verts_.begin(); it != verts_.end(); ++it)
+    {
+        Point3D p1 = *(it++), p2 = *it;
+        lines.push_back(Line3D(p1,p2));
+    }
+    return lines;
+}
+
+Cube::Cube()
+{
+    int newParameter = 100;
+    edgeLength = newParameter;
+    //front ?
+    verts_.push_back(Point3D(0,0,0));
+    verts_.push_back(Point3D(0,newParameter,0));
+    verts_.push_back(Point3D(0,newParameter,0));
+    verts_.push_back(Point3D(newParameter,newParameter,0));
+    verts_.push_back(Point3D(newParameter,newParameter,0));
+    verts_.push_back(Point3D(newParameter,0,0));
+    verts_.push_back(Point3D(newParameter,0,0));
+    verts_.push_back(Point3D(0,0,0));
+
+    //back
+    verts_.push_back(Point3D(0,0,newParameter));
+    verts_.push_back(Point3D(0,newParameter,newParameter));
+    verts_.push_back(Point3D(0,newParameter,newParameter));
+    verts_.push_back(Point3D(newParameter,newParameter,newParameter));
+    verts_.push_back(Point3D(newParameter,newParameter,newParameter));
+    verts_.push_back(Point3D(newParameter,0,newParameter));
+    verts_.push_back(Point3D(newParameter,0,newParameter));
+    verts_.push_back(Point3D(0,0,newParameter));
+
+    //top
+    verts_.push_back(Point3D(0,  newParameter,   0   ));
+    verts_.push_back(Point3D(0,  newParameter,   newParameter ));
+    verts_.push_back(Point3D(newParameter,newParameter,   0   ));
+    verts_.push_back(Point3D(newParameter,newParameter,   newParameter ));
+
+    //bottom
+    verts_.push_back(Point3D(0,   0,    0   ));
+    verts_.push_back(Point3D(0,   0,    newParameter ));
+    verts_.push_back(Point3D(newParameter, 0,    0   ));
+    verts_.push_back(Point3D(newParameter, 0,    newParameter ));
+
+    scale_factor_x = 1.0;
+    scale_factor_y = 1.0;
+    scale_factor_z = 1.0;
+}
+
+Cube::~Cube() { }
+
+Matrix4x4 Cube::getTransform() const
+{
+    Matrix4x4 centerOnGnomon = Matrix4x4(Vector4D(1,   0,  0,  -edgeLength*scale_factor_x/2  ),
+                                Vector4D(0,   1,  0,  -edgeLength*scale_factor_y/2  ),
+                                Vector4D(0,   0,  1,  -edgeLength*scale_factor_z/2  ),
+                                Vector4D(0,   0,  0,  1    ));
+
+    return this->translationTransform_ * rotationTransform_ * centerOnGnomon *this->getScaleTransform();
+}
+
+void Cube::resetTransform()
+{
+    this->gnomon.resetTransform();
+
+    this->translationTransform_= Matrix4x4();
+    this->rotationTransform_ = Matrix4x4();
+    this->setScaleX(1);
+    this->setScaleY(1);
+    this->setScaleZ(1);
+}
+void Cube::appendTranslationTransform(const Matrix4x4 &xform)
+{
+    this->gnomon.appendTranslationTransform(xform);
+    this->translationTransform_ = this->translationTransform_ * xform;
+}
+void Cube::appendRotationTransform(const Matrix4x4 &xform)
+{
+    this->gnomon.appendRotationTransform(xform);
+    this->rotationTransform_ = this->rotationTransform_ * xform;
+}
+
+void Cube::setScaleX(float scale)
+{
+    scale_factor_x = scale;
+}
+void Cube::setScaleY(float scale)
+{
+    scale_factor_y = scale;
+}
+void Cube::setScaleZ(float scale)
+{
+    scale_factor_z = scale;
+}
+
+Matrix4x4 Cube::getScaleTransform() const
+{
+    return Matrix4x4(Vector4D(scale_factor_x, 0, 0, 0),
+                     Vector4D(0, scale_factor_y, 0, 0),
+                     Vector4D(0, 0, scale_factor_z, 0),
+                     Vector4D(0, 0, 0, 1));
+}
+
+std::vector<Line3D> Cube::getLines()
+{
+    std::vector<Line3D> lines;
+
+    for(std::vector<Point3D>::iterator it = verts_.begin(); it != verts_.end(); ++it)
+    {
+        Point3D p1 = *(it++), p2 = *it;
+        lines.push_back(Line3D(p1,p2));
+    }
+    return lines;
+}
+
+
+
+
+
+Sphere::Sphere(Vector3D c, float r, Vector3D sc, Vector3D ec, float reflect, float transp)
+{
+    center = c;
+    radius = r;
+    radius2 = r*r;
+    surfaceColour = sc;
+    emissionColour = ec;
+    reflectivity = reflect;
+    transparency = transp;
+}
+Sphere::~Sphere() { }
--- a4/main.cpp
+++ a4/main.cpp
@@ -1,5 +1,33 @@
 #include <QImage>
 #include <QColor>
+#include <math.h>
+#include <vector>
+#include "algebra.h"
+
+using namespace std;
+
+vector<Shape*> shapes;
+//bgColour = Vector3D()
+//Vector3D normal(Point3D q)
+
+Vector3D trace(Point3D origin, Vector3D direction, int depth)
+{
+    Vector3D local, reflected, transmitted;
+    Point3D q; //intersection point
+    Vector3D n, r , t; // normal, reflection, transmission;
+
+//    if (depth > max) return bgColour;
+
+//    n = normal(q);
+//    r = reflect(q, n);
+//    t = transmit(q, n);
+//    local = phong(1, n, r);;  // and shadows
+
+//    reflected = trace(q, r, depth+1);
+//    transmitted = trace(q, t, depth+1);
+//    return local + reflected + transmitted;
+    return Vector3D();
+}
 
 int main(int argc, char *argv[])
 {
@@ -9,11 +37,17 @@ int main(int argc, char *argv[])
 
 	// image width and height
 	// TODO: prompt user on command line for dimensions
-	int width = 200;
-	int height = 200;
+    int width = 400;
+    int height = 400;
 
 	// create new image
 	QImage image(width, height, QImage::Format_RGB32);
+
+    // Spherere center, radius, colour, Vector3D surfaceColour, emissionColour; float transparency, reflectivity;
+    shapes.push_back((new Sphere(Vector3D(0,0,-5), 3, Vector3D(1,1,1), Vector3D(1,1,1)))); //big one at the bottom
+    shapes.push_back((new Sphere(Vector3D(0,5,-5), 0.5, Vector3D(0.8,0.75,1), Vector3D(1,1,1)))); // medium one at the top
+//    shapes.push_back(new Sphere(Vector3D(0,0,-20), 4, Vector3D(1,0.32,0.36), Vector3D(1,1,1)));
+
 
 	// iterate over the pixels & set colour values
 	for (int x = 0; x < width; x++)
@@ -30,11 +64,39 @@ int main(int argc, char *argv[])
 			if (x < width / 2 && y > height / 2) g = 1.0;
 			if (x > width / 2 && y > height / 2) b = 1.0;
 
+            float aspectratio = width / float(height);
+            float fov = 30;
+            float angle = tan(M_PI * 0.5 * fov / 180);
+
+            float xx = (2 * ((x + 0.5) * 1/float(width)) - 1) * angle * aspectratio;
+            float yy = (1 - 2 * ((y + 0.5) * (1/float(height))) * angle);
+            Vector3D ray = Vector3D(xx, yy, -1);
+            ray.normalize();
+
+            for (int i =0; i< shapes.size(); i++)
+            {
+                if (shapes[i]->intersect(Vector3D(), ray))
+                {
+                    r = 1;
+                    g = 1;
+                    b = 1;
+                }
+            }
+            // try to generate circle at (0,0) diameter half height)
+            if (pow(x-width/2,2) + pow(y-height/2,2) <= pow(height/10,2))
+            {
+                r = 0.5;
+                g = 0.5;
+                b = 0.5;
+            }
+
 			// set pixel value
 			image.setPixel(x, y, 
 				qRgb(r * 255, g * 255, b * 255));
 		}
 	}
+
+
 
 	// save to file
 	// TODO: prompt user on command line for output name
